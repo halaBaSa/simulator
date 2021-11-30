f62a50fe67bfa55d7a425ddcb8af6f8c64f90451
Hala <baridhala@gmail.com>
2021-11-30 14:34:33 +0100

    service event

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const b1 = document.querySelector('#b1');
+/*const b1 = document.querySelector('#b1');
 const b2 = document.querySelector('#b2');
 const b3 = document.querySelector('#b3');
 const b4 = document.querySelector('#b4');
@@ -12,8 +12,50 @@ const b9 = document.querySelector('#b9');
 const plus400 = document.querySelectorAll('.plus4');
 const minus400 = document.querySelectorAll('.minus4');
 const totaleCost = document.querySelector('.estimate-price')
-const plus = document.querySelector(".plus");
+const plus = document.querySelector(".plus");*/
+
+
+
+
+
+
+const s1 = document.getElementById("s1");
+const s2 = document.getElementById("s2");
+const s3 = document.getElementById("s3");
+const s4 = document.getElementById("s4");
+const s5 = document.getElementById("s5");
+const s6 = document.getElementById("s6");
+const totaleCost = document.querySelector('.estimate-price')
+
+
 let estimation = 0;
+
+
+
+s2.addEventListener('click', () => {
+    estimation += 1000;
+    totaleCost.innerHTML = estimation;
+});
+
+s3.addEventListener('click', () => {
+    estimation += 1500;
+    totaleCost.innerHTML = estimation;
+});
+
+s4.addEventListener('click', () => {
+    estimation += 2000;
+    totaleCost.innerHTML = estimation;
+});
+
+s5.addEventListener('click', () => {
+    estimation += 2500;
+    totaleCost.innerHTML = estimation;
+});
+
+s6.addEventListener('click', () => {
+    estimation += 3000;
+    totaleCost.innerHTML = estimation;
+});
 
 /*
 b1.addEventListener('click', () => { 
@@ -65,7 +107,7 @@ b9.addEventListener('click', () => {
 
 
 
-var OnClickEventFn = function(Element, PriceAction) {
+/*var OnClickEventFn = function(Element, PriceAction) {
         Element.addEventListener('click', () => {
             estimation = estimation + parseInt(PriceAction, 10);
             totaleCost.innerText = estimation;
@@ -88,9 +130,9 @@ var startSum = function() {
         /*var price = onServices[i].querySelector(".valeur");
         estimation = estimation + parseInt(price, 10);
         totaleCost.innerText = estimation;
-        console.log(estimation, price);*/
+        console.log(estimation, price);
 
-    }
+}
 }
 
 plus.addEventListener('click', () => {
@@ -105,4 +147,34 @@ plus.addEventListener('click', () => {
 
 });
 
-startSum();
+startSum();*/
+
+// input 
+
+const minus = document.querySelector(".minus");
+const plus = document.querySelector(".plus");
+const lInput = document.querySelector("input[name ='quantite']");
+var event = new Event('change');
+
+lInput.addEventListener('change', () => {
+    var count = lInput.value;
+    estimation = count * 500;
+    totaleCost.innerHTML = estimation;
+    console.log(count);
+
+})
+
+minus.addEventListener('click', () => {
+    var count = parseInt(lInput.value) - 1;
+    count = count < 0 ? 0 : count;
+    lInput.value = isNaN(count) ? 0 : count;
+    lInput.dispatchEvent(event);
+
+})
+
+plus.addEventListener('click', () => {
+    var count = parseInt(lInput.value) + 1;
+    lInput.value = isNaN(count) ? 0 : count;
+    lInput.dispatchEvent(event);
+
+})
